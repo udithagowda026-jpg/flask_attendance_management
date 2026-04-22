2040d6b091ae50eb91869eca575e6cf9d768bd7f
uditha <udithagowda026@gmail.com>
2026-04-22 11:09:27 +0530

    hi

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("Home.html")
+
+@app.route("/about")
+def about():
+    return render_template("About.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("Signup.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("Login.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
